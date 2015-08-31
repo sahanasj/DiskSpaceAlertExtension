4bcf653453b7437d6d8f81ff2c030f7316691ac1
Derrek Young <derrek.young@appdynamics.com>
2015-08-31 15:44:10 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# DiskSpaceAlertExtension
+Author: derrek.young@appdynamics.com
+
+# Sumary
 An AppDynamics Machine Agent extension to alert if disk space crosses a warning or critical threshold. If a threshold is crossed, the extension will create a custom event upon which you can trigger a Policy + Action to get notified.
+
+# Configuration
+`check_disk.cf`g contains the settings for the Warning and Critical thresholds as well as the Controller connection information.
+
+Set DEBUG=true to enable echo statements that are useful for debugging the script and config. These statements are not printed to the agent log file so you'll have to run teh script directly if you're debugging.
+
+Set MINUTE_FREQUENCY to change how often the script will run.
+
+The other settings should be self-explanatory.
+
+## Policies and Actions
+Refer to the screenshots and PowerPoint file in the Docs/ directory.
+
+The script will create a Custom Event if a threshold is crossed. You can create a Policy to trigger based on the Custom Event's `level` property. You can also use the Config Exporter tool to import the included policies.json file.
+https://docs.appdynamics.com/display/PRO41/Configure+Policies#ConfigurePolicies-ConfigurePolicyTriggers
+
+You'll want to test and validate that the extension is working so make sure to go to the Events page and specify the `DiskSpace` event type in the search criteria.
+
+# Usage
+
+## Deploy as An Extension
+Copy the runtime files to the `<MACHINE_AGENT_HOME>/monitors/` directory. Your final deployment should look something like <MACHINE_AGENT_HOME>/monitors/DiskSpaceAlertExtension/
+
+## Run Manually
+./check_disk.sh
+
+# Resources
+https://docs.appdynamics.com/display/PRO41/Extensions+and+Custom+Metrics
+
+https://docs.appdynamics.com/display/PRO41/Build+a+Monitoring+Extension+Using+Scripts
---
+++ check_disk.sh
@@ -0,0 +1,124 @@
+#!/bin/bash
+# set -x
+############################################################
+# A script to check the available space of the various
+# filesystems on a Linux server.
+#
+# Author: derrek.young@appdynamics.com
+#
+############################################################
+
+# The configuration file
+. ./check_disk.cfg
+
+# URL encode the data
+urlencode() {
+    # urlencode <string>
+    local length="${#1}"
+    for (( i = 0; i < length; i++ )); do
+        local c="${1:i:1}"
+        case $c in
+            [a-zA-Z0-9.~_-]) printf "$c" ;;
+            *) printf '%%%02X' "'$c"
+        esac
+    done
+}
+
+# Create the custom event via cURL
+function send_custom_event {
+  local level=$1
+  local summary=$2
+  local comment=$3
+  local app=$(urlencode "$APPLICATION")
+
+  local data="eventtype=CUSTOM&customeventtype=DiskSpace&summary=$summary&comment=$comment&severity=$level&propertynames=level&propertyvalues=$level"
+
+  local curl_cmd="curl --user $USER:$PASSWORD --data \"$data\" $CONTROLLER_URL/controller/rest/applications/$app/events"
+
+  if [ "$DEBUG" = true ] ; then
+    echo "Sending cURL to create event."
+    echo "level: $level"
+    echo "summary: $summary"
+    echo "comment: $comment"
+    echo "data: $data"
+    echo " "
+    echo $curl_cmd
+    echo " "
+  fi
+
+	eval $curl_cmd
+  echo ""
+}
+
+# Compare the disk space used to the thresholds
+function check_disk_used {
+  while read output;
+  do
+    local filesystem=$(echo $output | awk '{print $1}')
+    local size=$(echo $output | awk '{print $2}')
+    local usep=$(echo $output | awk '{ print $5}' | cut -d'%' -f1)
+    local partition=$(echo $output | awk '{print $6}')
+
+    local message="Host: $HOSTNAME, Filesystem: $filesystem, Partition: $partition, Percent Used: $usep%, Application: $APPLICATION"
+
+    if [ $usep -ge $CRITICAL_USED ] ; then
+      if [ "$DEBUG" = true ] ; then
+        echo "$output"
+        echo "ERROR: $message"
+      fi
+
+      # Create the event but URL encode the data first
+      send_custom_event "ERROR" $(urlencode "Disk space critical. $message") ""
+
+    elif [ $usep -ge $WARNING_USED ] ; then
+      if [ "$DEBUG" = true ] ; then
+        echo "$output"
+        echo "WARN: $message"
+      fi
+
+      # Create the event but URL encode the data first
+      send_custom_event "WARN" $(urlencode "Disk space warning. $message") ""
+
+    else
+      if [ "$DEBUG" = true ] ; then
+        echo "$output"
+        echo "Filesystem is good."
+        echo " "
+      fi
+    fi
+  done
+}
+
+# Check the required variables are defined in diskspace.cfg
+function check_variables {
+  for var in WARNING_USED CRITICAL_USED CONTROLLER_URL USER PASSWORD APPLICATION MINUTE_FREQUENCY ; do
+    if [ -n "${!var}" ] ; then
+      if [ "$DEBUG" = true ] ; then
+        echo "$var=${!var}"
+      fi
+    else
+        echo "$var variable is required. Please specify this in a file named diskspace.cfg that is in the same folder as this script."
+        exit
+    fi
+  done
+}
+
+function main {
+  check_variables
+
+  minute=0
+  while [ 1 ]; do
+    if [ $(( $minute % $MINUTE_FREQUENCY )) -eq 0 ]; then
+      if [ "$EXCLUDE_LIST" != "" ] ; then
+        df -H | grep -vE "^Filesystem|tmpfs|cdrom|${EXCLUDE_LIST}" | check_disk_used
+      else
+        df -H | grep -vE "^Filesystem|tmpfs|cdrom" | check_disk_used
+      fi
+    fi
+    sleep 60
+    minute=`expr $minute + 1`
+  done
+}
+
+# Run the main script
+main
